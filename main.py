b6e5222e0e345e00a39b986fd1b352d0439e9796
wonch01 <wonch426@naver.com>
2023-11-13 11:20:04 +0900

    1

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import sample
 
 def main():
     try:
-        # sample.open_site()
         config = sample.load_config()
-        conn, cur = sample.postgres_con(config)
-        sample.create_table(conn,cur)
-        sample.insert_data(conn,cur)
-        sample.postgres_close(conn, cur)
-
+        conn, cur, engine = sample.postgres_con(config)
+        sample.create_table(conn, cur)
+        sample.insert_data(conn, cur)
+        sample.show_data(engine)
+        # sample.drop_table('sample_data', cur)
 
     except Exception as error:
         print("error:", error)
         return False
-
+    finally:
+        sample.postgres_close(conn, cur)
 
 if __name__ == '__main__':
     main()
--- sample.py
+++ sample.py
@@ -1,20 +1,7 @@
 import psycopg2
 import json
-import requests
-
-
-def open_site():
-    url = "https://www.postgresql.org/download/"
-    try:
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            print(response.text)
-        else:
-            print(f"HTTP 요청 실패. 상태 코드: {response.status_code}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"오류 발생: {e}")
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_config(filename='config.json'):
@@ -38,7 +25,8 @@ def postgres_con(config):
 
     pg_conn = psycopg2.connect(host=host, dbname=db, user=user, password=password, port=port)
     pg_cur = pg_conn.cursor()
-    return pg_conn, pg_cur
+    pg_engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}")
+    return pg_conn, pg_cur, pg_engine
 
 
 def postgres_close(pg_conn, pg_cur):
@@ -47,44 +35,44 @@ def postgres_close(pg_conn, pg_cur):
     pg_cur.close()
 
 
-def create_table(conn,cur):
+def create_table(conn, cur):
     create_table_sql = """
     CREATE TABLE IF NOT EXISTS sample_data (
         coordinates integer[],
         text_data text,
         numeric_data double precision
     );
-    
+
     """
     cur.execute(create_table_sql)
     conn.commit()
 
 
-def insert_data(conn,cur):
+def insert_data(conn, cur):
     data = [
-        (   [[24, 8], [110, 8], [110, 16], [24, 16]],
-            '미로나스국T리! 괴갑대쏘다',
-            0.0017230819330160733),
-        (   [[10, 16], [56, 16], [56, 24], [10, 24]],
-            '(P못 피스a(끼기',
-            0.006269590404310938),
+        ([[24, 8], [110, 8], [110, 16], [24, 16]],
+         '미로나스국T리! 괴갑대쏘다',
+         0.0017230819330160733),
+        ([[10, 16], [56, 16], [56, 24], [10, 24]],
+         '(P못 피스a(끼기',
+         0.006269590404310938),
         ([[91, 73], [143, 73], [143, 89], [91, 89]], '사갑럽니다', 0.1783492257449009),
-        (   [[91, 91], [223, 91], [223, 105], [91, 105]],
-            '바람이념신물더러온 그순간부터 ',
-            0.10488713129482491),
-        (   [[170, 290], [200, 290], [200, 296], [170, 296]],
-            '거파y3 너',
-            0.017439958583245804),
-        (   [[99, 297], [167, 297], [167, 311], [99, 311]],
-            '미아지키하아오',
-            0.6818672154523849),
-        (   [[43, 313], [251, 313], [251, 375], [43, 375]],
-            '바람이 붙다',
-            0.6794710701367007),
-        (   [[65, 391], [223, 391], [223, 405], [65, 405]],
-            '9리s임 가슴-속데간되다 그리움음 만니세인',
-            0.012198750988838075)
-        ]
+        ([[91, 91], [223, 91], [223, 105], [91, 105]],
+         '바람이념신물더러온 그순간부터 ',
+         0.10488713129482491),
+        ([[170, 290], [200, 290], [200, 296], [170, 296]],
+         '거파y3 너',
+         0.017439958583245804),
+        ([[99, 297], [167, 297], [167, 311], [99, 311]],
+         '미아지키하아오',
+         0.6818672154523849),
+        ([[43, 313], [251, 313], [251, 375], [43, 375]],
+         '바람이 붙다',
+         0.6794710701367007),
+        ([[65, 391], [223, 391], [223, 405], [65, 405]],
+         '9리s임 가슴-속데간되다 그리움음 만니세인',
+         0.012198750988838075)
+    ]
 
     insert_data_sql = """
             INSERT INTO sample_data (coordinates, text_data, numeric_data)
@@ -94,3 +82,14 @@ def insert_data(conn,cur):
         cur.execute(insert_data_sql, (item[0], item[1], item[2]))
     conn.commit()
 
+
+def drop_table(name, cur):
+    query = f"drop table if exists {name}"
+    cur.execute(query)
+
+
+def show_data(pg_engine):
+    query = "select * from sample_data"
+    pd.set_option('display.max_columns', None)
+    df = pd.read_sql_query(query, pg_engine)
+    print(df)
